fedbe685b48c681ea3ea419d92bdb632883b0ff0
tkzc00 <zqzkzy2420586708@qq.com>
2022-09-27 18:23:54 +0800

    创建选手类

---
+++ Speaker.cpp
@@ -0,0 +1,2 @@
+#include "Speaker.h"
+
---
+++ Speaker.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+// 选手类
+class Speaker
+{
+public:
+
+	string name;
+	double score[2]; // 分数，最多有两轮的得分
+};
+
